46bc8a7640d3113d33bc74e8e83f0b7216a4f561
johnhhancock <hancockherbertjohn@gmail.com>
2019-07-20 15:36:13 -0400

    Update upload.js

--- server/upload.js
+++ server/upload.js
@@ -5,7 +5,7 @@ const path = require('path');
 const extract = require('extract-zip');
 const spawn = require('child_process').spawn;
 
-var upload_path = __dirname + '/uploads//';
+var upload_path = __dirname + '/uploads/';
 var image_path = __dirname + '/images/';
 var python_script = __dirname + '/sort_images.py';
 var sorted_zip = __dirname + '/sorted.zip';
